ce111a480f72fe6da447499b7b1328ff344b914a
piotrszmurlo <piotr.szmurlo7@gmail.com>
2022-01-17 02:16:49 +0100

    all functionality added

--- t6/hello.c
+++ t6/hello.c
@@ -91,11 +91,14 @@ int read_superblock(int fildes) {
     int used_inodes[MAX_FILE_COUNT]; 
     lseek(fildes, 0, SEEK_SET);
     read(fildes, &gsb->filesystemSize, sizeof(unsigned int));
+    lseek(fildes, 4, SEEK_SET);
     read(fildes, &gsb->blockSize, sizeof(unsigned int));
+    lseek(fildes, 8, SEEK_SET);
     read(fildes, &gsb->usedBlocksCount, sizeof(unsigned int));
+    lseek(fildes, 12, SEEK_SET);
     read(fildes, &gsb->freeBlocksCount, sizeof(unsigned int));
+    lseek(fildes, 16, SEEK_SET);
     read(fildes, &gsb->inodeCount, sizeof(unsigned int));
-    read(fildes, gsb->blockMap, sizeof(unsigned int)*BLOCK_COUNT);
     int offset = 5*sizeof(unsigned int);
     lseek(fildes, offset, SEEK_SET);
     
@@ -107,30 +110,17 @@ int read_superblock(int fildes) {
     offset = offset + sizeof(gsb->inodes);
     lseek(fildes, offset, SEEK_SET);
     read(fildes, &(gsb->used_inodes), MAX_FILE_COUNT*sizeof(int));
-    lseek(fildes, 0, SEEK_SET);
+    read(fildes, gsb->blockMap, sizeof(gsb->blockMap));
 
 }
 
 int check_space(int fildes, unsigned int fileSize) {
-    // superblock *sb = malloc(sizeof(superblock));
-    
-    // lseek(fildes, 0, SEEK_SET);
-    // sb->filesystemSize = 4;
-    // char buf[772];
-    // read(fildes, buf, 772);
-    // int i = 0;
-    // for (i=0;i<1;i++){
-    //     printf("%i", (int)buf[i]);
-    // }
-    // read(fildes, &sb->filesystemSize, sizeof(sb->filesystemSize));
-    // read(fildes, &sb->filesystemSize, sizeof(sb->filesystemSize));
-    // printf("HUJ: %d", gsb->filesystemSize);
-    if (gsb->inodeCount == MAX_FILE_COUNT){
-        printf("error copying file: max files num on vdisk");
+    if (gsb->inodeCount > MAX_FILE_COUNT - 1){
+        printf("error copying file: max files num on vdisk\n");
         return -1;
     }
     if ((gsb->freeBlocksCount)*BLOCK_SIZE < fileSize){
-        printf("error copying file: not enough space on vdisk");
+        printf("error copying file: not enough space on vdisk\n");
         return -1;
     }
     return 0;
@@ -154,8 +144,7 @@ int copy_file_to_vfs(int fildes, char* fileName) {
         }
     }
     printf("i: %d\n", i);
-    gsb->used_inodes[i] = 1;
-    gsb->inodeCount--;
+    gsb->inodeCount++;
     inode* newInode = malloc(sizeof(inode));
     strncpy(newInode->fileName, fileName, MAX_FILENAME_LENGTH-1);
     newInode->fileSize = stbuf.st_size;
@@ -166,10 +155,11 @@ int copy_file_to_vfs(int fildes, char* fileName) {
     for (int i = 0; i < MAX_FILE_COUNT; i++){
         if (!gsb->used_inodes[i]){
             gsb->inodes[i] = *newInode;
+            break;
         }
     }
+    gsb->used_inodes[i] = 1;
     lseek(fildes, (newInode->firstDataBlock)*BLOCK_SIZE + sizeof(superblock), SEEK_SET);
-    // char buf[BLOCK_SIZE-4];
     int rd = read(newFile, newBlock->data, BLOCK_SIZE-4);
     while (rd)
     {
@@ -208,29 +198,138 @@ int print_files(int fildes){
     read(fildes, &used_inodes, MAX_FILE_COUNT*sizeof(int));
     for (int i = 0; i < MAX_FILE_COUNT; i++){
         if (used_inodes[i])
-        printf("PLIK: %s\n", (&inodes[i+1])->fileName);
+        printf("PLIK: %s\n", (&inodes[i])->fileName);
     }
 }
 
+int print_block_map(int fildes) {
+    read_superblock(fildes);
+    printf("|Block index : State|\n");
+    for (int i = 1; i < BLOCK_COUNT+1; i++) {
+        if (gsb->blockMap[i]) {
+            printf("|%d : used", i);
+        }
+        else {
+            printf("|%d : free", i);
+        }
+        if (i%5 == 0) printf("\n");
+    }
+}
 
+int copy_file_from_vfs(int fildes, char* fileName) {
+    read_superblock(fildes);
+    int found = 0;
+    inode* fileNode;
+    for (int i = 0; i < MAX_FILE_COUNT; i++){
+        if (strcmp(fileName, gsb->inodes[i].fileName) == 0) {
+            found = 1;
+            fileNode = &gsb->inodes[i];
+        }
+    }
+    if (!found) {
+        printf("File not found in vdisk");
+        return -1;
+    }
+    int outFile = open(fileName, O_WRONLY | O_CREAT);
+    // int offset = sizeof(superblock) + fileNode->firstDataBlock*BLOCK_SIZE;
+    block* buffer = malloc(sizeof(block));
+    buffer->nextBlock = fileNode->firstDataBlock;
+    do {
+        lseek(fildes, buffer->nextBlock*BLOCK_SIZE + sizeof(superblock), SEEK_SET);
+        read(fildes, buffer->data, BLOCK_SIZE-4);
+        read(fildes, &buffer->nextBlock, sizeof(unsigned int));
+        write(outFile, buffer->data, BLOCK_SIZE-4);
+    } while (buffer->nextBlock != -1);
+    close(outFile);
+    free(buffer);
+    return 0;
+}
 
+int delete_file(int fildes, char* fileName) {
+    read_superblock(fildes);
+    int found = 0;
+    inode* fileNode;
+    for (int i = 0; i < MAX_FILE_COUNT; i++){
+        printf("res: %s\n", gsb->inodes[i].fileName);
+        if (strcmp(fileName, gsb->inodes[i].fileName) == 0 && gsb->used_inodes[i] == 1) {
+            found = 1;
+            fileNode = &gsb->inodes[i];
+            gsb->used_inodes[i] = 0;
+        }
+    }
+    if (!found) {
+        printf("File not found in vdisk\n");
+        return -1;
+    }
+    block* buffer = malloc(sizeof(block));
+    buffer->nextBlock = fileNode->firstDataBlock;
+    for (int i = 0; i < BLOCK_SIZE-4; i++) {
+        buffer->data[i] = '\0';
+    }
+    // int temp;
+    const unsigned int zero = 0;
+    do {
+        gsb->freeBlocksCount++;
+        gsb->usedBlocksCount--;
+        gsb->blockMap[buffer->nextBlock] = 0;
+        lseek(fildes, buffer->nextBlock*BLOCK_SIZE + sizeof(superblock), SEEK_SET);
+        // temp = buffer->nextBlock;
+        write(fildes, buffer->data, BLOCK_SIZE-4);
+        read(fildes, &buffer->nextBlock, sizeof(unsigned int));
+        lseek(fildes, (buffer->nextBlock+1)*BLOCK_SIZE + sizeof(superblock) - sizeof(unsigned int), SEEK_SET);
+        write(fildes, &zero, sizeof(unsigned int));
+    } while (buffer->nextBlock != -1);
+    gsb->filesystemSize = gsb->filesystemSize - fileNode->fileSize;
+    gsb->inodeCount--;
+    free(buffer);
+    rewrite_superblock(fildes);
+}
+
+int delete_vdisk(char* filename, int fildes){
+    close(fildes);
+    if (remove(filename) == 0)
+      printf("vdisk eleted successfully");
+   else
+      printf("error deleting vdisk");
+   return 0;
+}
 
 int main(int argc, char** argv) {
     char *filename = "vdisk";
     vdiskID = open(filename, O_RDWR | O_CREAT);
 
-    create_vfs();
-    check_space(vdiskID, 100);
-    printf("ilosc %d\n", gsb->inodeCount);
-    // copy_file_to_vfs(vdiskID, "koxakox");
-    // copy_file_to_vfs(vdiskID, "koxbkox");
+    // create_vfs();
+    superblock* sb = malloc(sizeof(superblock));
+    gsb = sb;
     read_superblock(vdiskID);
     rewrite_superblock(vdiskID);
-    // copy_file_to_vfs(vdiskID, "koxckox");
-    // copy_file_to_vfs(vdiskID, "koxckox");
+
+    // check_space(vdiskID, 100);
+    printf("ilosc %d\n", gsb->inodeCount);
+    if(copy_file_to_vfs(vdiskID, "koxbkox")){
+        return -1;
+    }    
+    // printf("ilosc %d\n", gsb->inodeCount);
+    // if(copy_file_to_vfs(vdiskID, "koxbkox")){
+    //     return -1;
+    // }    
+    // printf("ilosc %d\n", gsb->inodeCount);
+    // if(copy_file_to_vfs(vdiskID, "koxckox")){
+    //     return -1;
+    // }
+    delete_file(vdiskID, "koxbkox");
+    printf("ilosc %d\n", gsb->inodeCount);
+    for (int i =0;i<MAX_FILE_COUNT; i++) {
+        printf("usedinode: %d->%d\n", i, gsb->used_inodes[i]);
+    }
+    // copy_file_from_vfs(vdiskID, "koxckox");
+    
+    rewrite_superblock(vdiskID);
+    read_superblock(vdiskID);
     print_files(vdiskID);
+    // print_block_map(vdiskID);
     close(vdiskID);
+    delete_vdisk("vdisk", vdiskID);
 
 }
-///
 
